fe1ad126ae33b3a468462f679b370a979a3d4368
ramshadKen4 <ramshadken@icloud.com>
2024-04-18 03:04:10 +0530

    divide sections as heder main and footer

---
+++ src/Layouts/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Logo from '../components/Header/Logo'
+import TiltleImage from '../components/Header/TiltleImage'
+import Placement from '../components/Header/Placement'
+import './Header.css'
+
+function Header() {
+    return (
+        <div className='header'>
+            <Logo />
+            <TiltleImage/>
+            <Placement/>
+        </div>
+    )
+}
+
+export default Header
