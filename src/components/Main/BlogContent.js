9437f077ddc0578a2c5de9d06e4c4b5068b09f1c
ramshadKen4 <ramshadken@icloud.com>
2024-04-18 14:51:24 +0530

    blog content created

---
+++ src/components/Main/BlogContent.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './BlogContent.css'
+
+function BlogContent() {
+    return (
+        <div className="blog-content">
+            <h2 className="blog-heading">Why Choose Us ?</h2>
+            <p className="blog-paragraph">
+                People click Learning’s Python Full Stack development Course provides the complete practical live project training in Python Development. From Basics to Advanced Level, our Python Full Stack Development Training is provided by our industry experts. Based on the requirements of the students to achieve their goals, we have built our sophisticated Full Stack with Python Django Training Course curriculum. We provide both in-person Python Full Stack development course instruction and online Python Full Stack instruction with real-time projects.
+            </p>
+        </div>
+    )
+}
+
+export default BlogContent
