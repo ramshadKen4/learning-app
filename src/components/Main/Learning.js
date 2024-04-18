d3f945d43b0c773a06b9d49668d31b8b1608622a
ramshadKen4 <ramshadken@icloud.com>
2024-04-18 17:22:26 +0530

    completed post card

--- src/components/Main/Learning.js
+++ src/components/Main/Learning.js
@@ -11,13 +11,13 @@ function Learning() {
                 image="title-image.png"
                 caption="Our Full stack development program is offered on both online and offline classes"
             >
-                <ul>
-                    <li>Weekly live instruction classes</li>
-                    <li>Hands-on live projects</li>
-                    <li>Industry-relevant live bootcamps</li>
-                    <li>Industry-Recognized certificates</li>
-                    <li>Participating in practice interviews & CV preparation</li>
-                    <li>Getting ready for placements</li>
+                <ul className='blog-para'>
+                    <li><img src="/icons/check-mark.png" alt="Check Mark Icon" />Weekly live instruction classes</li>
+                    <li><img src="/icons/check-mark.png" alt="Check Mark Icon" />Hands-on live projects</li>
+                    <li><img src="/icons/check-mark.png" alt="Check Mark Icon" />Industry-relevant live bootcamps</li>
+                    <li><img src="/icons/check-mark.png" alt="Check Mark Icon" />Industry-Recognized certificates</li>
+                    <li><img src="/icons/check-mark.png" alt="Check Mark Icon" />Participating in practice interviews & CV preparation</li>
+                    <li><img src="/icons/check-mark.png" alt="Check Mark Icon" />Getting ready for placements</li>
                 </ul>
             </PostCard>
         </div>
