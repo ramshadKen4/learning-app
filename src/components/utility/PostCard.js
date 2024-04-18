5c13c0817a80c83920e746f5b6288c9f5dce71a1
ramshadKen4 <ramshadken@icloud.com>
2024-04-18 16:56:07 +0530

    create post card componets for every post cards

---
+++ src/components/utility/PostCard.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './PostCard.css'
+function PostCard({ heading, caption, image, children }) {
+    return (
+        <div className='post-card'>
+            <div className='post-header'>
+                <h2>{heading}</h2>
+                <p>{caption}</p>
+            </div>
+            <div className='post-content'>
+                <img src={'/images/'+image} alt='Post' />
+                <div>{children}</div>
+            </div>
+        </div>
+    )
+}
+
+export default PostCard
