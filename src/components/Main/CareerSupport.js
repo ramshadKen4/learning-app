c2ac179f12afc88f4283976739a1099243bf07b5
ramshadKen4 <ramshadken@icloud.com>
2024-04-20 16:52:01 +0530

    added career support post (grow with learning)

---
+++ src/components/Main/CareerSupport.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import PostCard from '../utility/PostCard'
+
+function CareerSupport() {
+    return (
+        <div className='learning'>
+
+            <PostCard
+                heading="Grow with People click Learning"
+                image="career-support.png"
+            >
+                <p className='blog-para'>
+                    At People click , we want to give you a thorough educational experience.
+                    Because of our special career support services,
+                    we are here to support you even after you start working and have a new perspective.
+                    Gain access to over 500 placement partners and explore an abundance of opportunities.
+                </p>
+            </PostCard>
+        </div>
+    )
+}
+
+export default CareerSupport
