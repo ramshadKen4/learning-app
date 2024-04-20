c8ec187309cf2777121c42209532e7032559e987
ramshadKen4 <ramshadken@icloud.com>
2024-04-20 16:54:07 +0530

    create trainer profile

---
+++ src/components/Main/TrainingProfile.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import PostCard from '../utility/PostCard'
+
+function TrainingProfile() {
+    return (
+        <div className='learning'>
+            <PostCard
+                heading="Our Full-stack developer trainer profile"
+            >
+                <p className='blog-para'>
+                    Our Experienced specialists with a focus on many facets of Full Stack Development, such as front-end, back end, Angular, React, and other areas, teach at People click learning
+                    These instructors provide hands-on instruction to students in both client-side and server-side programming.
+                    The trainers evaluate each student on a regular basis to monitor their progress.
+                    By conducting practice interviews and providing insightful commentary on interview questions, the trainers are essential in helping students get ready for the workforce.
+                </p>
+            </PostCard>
+        </div>
+    )
+}
+
+export default TrainingProfile
