ae6504af4de0a57084007d66646eb211b731b711
ramshadKen4 <ramshadken@icloud.com>
2024-04-21 17:59:24 +0530

    create address component

---
+++ src/components/Footer/Address.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './Address.css';
+
+const Address = () => {
+    return (
+        <div className="address-container">
+            <div className="address-section">
+                <p className="address-text">
+                    Bangalore<br/>
+                    Bhumi Complex<br/>
+                    Sector 7, HSR Layout Bengaluru Karnataka<br/>
+                    560102
+                </p>
+                <div className="phone-numbers">
+                    <span>+91 7338 497 706</span>
+                    <span>+91 9741 838 817</span>
+                </div>
+            </div>
+            <div className="separator"></div>
+            <div className="address-section">
+                <p className="address-text">
+                    Coimbatore<br/>
+                    41A, 2nd floor West Lokamanya<br/>
+                    Street, RS Puram Coimbatore<br/>
+                    641002
+                </p>
+                <div className="phone-numbers">
+                    <span>+91 9164 161 200</span>
+                    <span>+91 8867 636 114</span>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Address;
