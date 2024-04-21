672e36d51f6b58f3770fb78f689983542e3d330e
ramshadKen4 <ramshadken@icloud.com>
2024-04-21 17:58:49 +0530

    add profile card for placement and testmonials

---
+++ src/components/utility/ProfileCard.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './ProfileCard.css'
+
+function ProfileCard({profilePic, name, company, bg, children}) {
+  return (
+    <div class="profile-card" style={{backgroundColor:bg}}>
+      <div class="profile-pic-container">
+        <div class="profile-pic">
+          <img src={"/icons/" + profilePic} alt="Profile Picture" />
+        </div>
+      </div>
+      <div class="client-info">
+        <h3 class="client-name">{name}</h3>
+        <p class="client-role">{company}</p>
+      </div>
+      <div class="profile-text">
+        {children}
+      </div>
+    </div>
+  )
+}
+
+export default ProfileCard
+
