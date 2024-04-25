7e08b3774e90739be471157fd16e32208eb213aa
ramshadKen4 <ramshadken@icloud.com>
2024-04-26 03:13:57 +0530

    create titile

--- src/components/Header/TiltleImage.js
+++ src/components/Header/TiltleImage.js
@@ -5,7 +5,10 @@ function TiltleImage() {
   return (
     <div className="title-image">
       <div className="title">
-        <img src='/images/title.png' alt="Title" />
+        <h4>Learn</h4>
+        <h1>Full stack</h1>
+        <h1>Development</h1>
+        <h5>Training Program</h5>
       </div>
       <div className="image">
         <img src='/images/title-image.png' alt="Title Image" />
