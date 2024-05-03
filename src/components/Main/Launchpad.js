3056a8141c69181565562974b5558549f6d75faf
ramshadKen4 <ramshadken@icloud.com>
2024-05-03 19:38:46 +0530

    change class to class name for launchpad

--- src/components/Main/Launchpad.js
+++ src/components/Main/Launchpad.js
@@ -5,22 +5,22 @@ import PostCard from '../utility/PostCard'
 
 function Launchpad() {
     return (
-        <div class="launchpad-container">
-            <div class="item">
+        <div className="launchpad-container">
+            <div className="item">
                 <h2 className='launchpad-heading'>Career Counseling</h2>
                 <p className='launchpad-para'>
                     Our devoted program mentors are seasoned professionals who will assist you with the readings,
-                    ensure that you are informed about what is happening in class, and motivate you to succeed academically.
+                    ensure that you are informed about what is happening in className, and motivate you to succeed academically.
                 </p>
             </div>
-            <div class="item">
+            <div className="item">
                 <h2 className='launchpad-heading'>Complete Placement Assistance</h2>
                 <p className='launchpad-para'>
                     With our career services,
                     we'll make sure you land your ideal position and introduce you to rewarding opportunities in digital marketing.
                 </p>
             </div>
-            <div class="item">
+            <div className="item">
                 <h2 className='launchpad-heading'>Getting Ready For The Interview</h2>
                 <p className='launchpad-para'>
                 We have created a training course that, via an analysis of the most often asked interview questions, will confidently prepare you for employment interviews.
