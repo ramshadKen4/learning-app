440a6939502ff47c94748c3f31f6791b85300009
ramshadKen4 <ramshadken@icloud.com>
2024-05-05 12:38:53 +0530

    set icon foe address phone number section

--- src/components/Footer/Address.js
+++ src/components/Footer/Address.js
@@ -6,30 +6,36 @@ const Address = () => {
         <div className="address-container">
             <div className="address-section">
                 <p className="address-text">
-                    Bangalore<br/>
-                    Bhumi Complex<br/>
-                    Sector 7, HSR Layout 
-                    Bengaluru Karnataka<br/>
+                    Bangalore<br />
+                    Bhumi Complex<br />
+                    Sector 7, HSR Layout
+                    Bengaluru Karnataka<br />
                     560102
                 </p>
-                <div className="phone-numbers">
-                    <span>+91 7338 497 706</span>
-                    <span>+91 9741 838 817</span>
+                <div className='phone_section'>
+                    <img src='/icons/call.svg'></img>
+                    <div className="phone-numbers">
+                        <span>+91 7338 497 706</span>
+                        <span>+91 9741 838 817</span>
+                    </div>
                 </div>
             </div>
             <div className="separator"></div>
             <div className="address-section">
                 <p className="address-text">
-                    Coimbatore<br/>
+                    Coimbatore<br />
                     41A, 2nd floor
-                    WestLokamanya<br/>
+                    WestLokamanya<br />
                     Street, RS Puram
-                    Coimbatore<br/>
+                    Coimbatore<br />
                     641002
                 </p>
-                <div className="phone-numbers">
-                    <span>+91 9164 161 200</span>
-                    <span>+91 8867 636 114</span>
+                <div className='phone_section'>
+                    <img src='/icons/call.svg'></img>
+                    <div className="phone-numbers">
+                        <span>+91 9164 161 200</span>
+                        <span>+91 8867 636 114</span>
+                    </div>
                 </div>
             </div>
         </div>
